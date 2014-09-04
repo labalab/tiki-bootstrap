ce40aa1c1fefe5f7c24b83d985858a734e6c630a
root <root@labalab.ca>
2014-09-04 11:33:24 -0400

    lessc -x fails

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir -p css
 mkdir -p templates
-lessc -x less/bootstrap.less css/tiki.css
+lessc less/bootstrap.less css/tiki.css
+# https://github.com/oyejorge/less.php/issues/126 - cannot use -x on clearos
 
